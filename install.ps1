4cb6850476e278e94d74692b67dc1cb0bfd776fe
Ayush Jha <ayush@wasmer.io>
2022-11-15 20:31:17 +0545

    install wapm

--- install.ps1
+++ install.ps1
@@ -11,6 +11,14 @@ if ($args.Length -eq 1) {
   $Version = $args.Get(0)
 }
 
+if ($w) {
+  $WapmVersion = "${w}"
+}
+if ($args.Length -eq 2) {
+  $WapmVersion = $args.Get(1)
+}
+
+
 $WasmerInstall = $env:WASMER_DIR
 $WasmerDir = if ($WasmerInstall) {
   "$WasmerInstall"
@@ -19,8 +27,10 @@ $WasmerDir = if ($WasmerInstall) {
 }
 
 $WasmerInstaller = "$Home\temp-wasmer-installer.exe"
+$WapmInstaller = "$Home\temp-wapm-installer.tar.gz"
 $WasmerBinDir = "$WasmerDir\bin"
 $Target = 'windows'
+$WapmTarget = 'windows-amd64'
 
 $allowedExecutionPolicy = @('Unrestricted', 'RemoteSigned', 'ByPass')
 if ((Get-ExecutionPolicy).ToString() -notin $allowedExecutionPolicy) {
@@ -36,14 +46,27 @@ if ((Get-ExecutionPolicy).ToString() -notin $allowedExecutionPolicy) {
 $WasmerInstallerUri = if (!$Version) {
   Write-Host "Fetching latest release..."
   $Response = Invoke-RestMethod -Uri 'https://api.github.com/repos/wasmerio/wasmer/releases/latest' -UseBasicParsing
-  ( 
-    $Response.assets | 
-    Where-Object { $_.name -eq "wasmer-${Target}.exe" } | 
-    Select-Object -First 1 
+  (
+    $Response.assets |
+    Where-Object { $_.name -eq "wasmer-${Target}.exe" } |
+    Select-Object -First 1
   ).browser_download_url
 } else {
   "https://github.com/wasmerio/wasmer/releases/download/${Version}/wasmer-${Target}.exe"
 }
+
+$WapmInstallerUri = if (!$WapmVersion) {
+  Write-Host "Fetching latest release..."
+  $Response = Invoke-RestMethod -Uri 'https://api.github.com/repos/wasmerio/wapm-cli/releases/latest' -UseBasicParsing
+  (
+    $Response.assets |
+    Where-Object { $_.name -eq "wasmer-${Target}.exe" } |
+    Select-Object -First 1
+  ).browser_download_url
+} else {
+  "https://github.com/wasmerio/wapm-cli/releases/download/${WapmVersion}/wasmer-${WapmTarget}.tar.gz"
+}
+
 
 if (!(Test-Path $WasmerDir)) {
   New-Item $WasmerDir -ItemType Directory | Out-Null
@@ -61,6 +84,20 @@ Write-Output "Installing Wasmer..."
 
 Start-Process $WasmerInstaller -Wait -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR=`"$WasmerDir`" /SP-"
 Remove-Item $WasmerInstaller
+
+# Install wapm
+Write-Host "Downloading wapm..."
+
+Invoke-WebRequest $WapmInstallerUri -OutFile $WapmInstaller -UseBasicParsing
+
+Write-Output "Installing wapm..."
+
+tar -xvf $WapmInstaller -C "$WasmerDir"
+
+Start-Process $WapmInstaller -Wait -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR=`"$WasmerDir`" /SP-"
+Remove-Item $WapmInstaller
+
+
 
 Write-Output "Adding Wasmer to ENV:Path $WasmerBinDir..."
 
